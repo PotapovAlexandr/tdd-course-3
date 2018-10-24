61ce693e839f8c91596a459f18b434715d11ded9
Alexander Potapov <potapov.alexander@dev.local>
2018-10-24 20:21:03 +0300

    Red fix mistake Signed-off-by: Alexander Potapov <potapov.alexander@dev.local>

--- tdd_intro/homework/04_weather_client/test.cpp
+++ tdd_intro/homework/04_weather_client/test.cpp
@@ -255,5 +255,5 @@ TEST (Weather, MaxTemperature_1_09 )
 {
     double max = 31;
     WeatherClient wClient;
-    ASSERT_EQ(max, wClient.GetMinimumTemperature(g_fakeServer, "02.09.2018"));
+    ASSERT_EQ(max, wClient.GetMinimumTemperature(g_fakeServer, "01.09.2018"));
 }
