5bf5c6b7aa50955ed00171c2dfe726842420fe54
PotapovAlexandr <alexpotapov1985@gmail.com>
2018-11-27 21:13:29 +0200

    Add Mocks. Signed-off-by: PotapovAlexandr <alexpotapov1985@gmail.com>

---
+++ tdd_intro/homework/07_sqlite_header_parser/Interfaces.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <string>
+
+struct SqliteHeader
+{
+    std::string head;
+    int pageSize;
+    bool fileFormatWriteVersion;
+    int bytesOfUnused;
+    int maximumEmbeddedPayloadFraction;
+    int minimumEmbeddedPayloadFraction;
+    int leafPayloadFraction;
+    int fileChangeCounter;
+    int pageCount;
+    int firstFreelistPage;
+    int freelistPageCount;
+//    40	4	The schema cookie.
+    int schemaFormat; // Supported schema formats are 1, 2, 3, and 4.
+    int defaultPageCacheSize;
+    int numberOfLargestRootPage;
+    int databaseTextEncoding; // A value of 1 means UTF-8. A value of 2 means UTF-16le. A value of 3 means UTF-16be.
+    int userVersion;
+    bool incrementalVacuumMode;
+    int applicationId;
+//    72	20	Reserved for expansion. Must be zero.
+    int versionValidNumber;
+    int sqliteVersionNumber;
+};
+
+class IGui
+{
+public:
+    virtual ~IGui(){}
+    virtual void DisplayHeader(const SqliteHeader& header) = 0;
+};
+
+class IDbReader
+{
+public:
+    virtual ~IDbReader(){}
+    virtual bool ReadFilePath(const std::string filePath) = 0;
+    virtual bool CheckHeader() = 0;
+    virtual std::string GetHeaderString() = 0;
+    virtual int GetPageSize() = 0;
+    virtual bool GetFormatWriteVersion() = 0;
+    virtual int GetUnsedBytes() = 0;
+    virtual int GetMaximumEmbeddedPayloadFraction() = 0;
+    virtual int GetMinimumEmbeddedPayloadFraction() = 0;
+    virtual int GetLeafPayloadFraction() = 0;
+    virtual int GetFileChangeCounter() = 0;
+    virtual int GetPageCount() = 0;
+    virtual int GetFirstFreelistPage() = 0;
+    virtual int GetFreelistPageCount() = 0;
+//    40	4	The schema cookie.
+    virtual int GetSchemaFormat() = 0; // Supported schema formats are 1, 2, 3, and 4.
+    virtual int GetDefaultPageCacheSize() = 0;
+    virtual int GetNumberOfLargestRootPage() = 0;
+    virtual int GetDatabaseTextEncoding() = 0; // A value of 1 means UTF-8. A value of 2 means UTF-16le. A value of 3 means UTF-16be.
+    virtual int GetUserVersion() = 0;
+    virtual bool GetIncrementalVacuumMode() = 0;
+    virtual int GetApplicationId() = 0;
+//    72	20	Reserved for expansion. Must be zero.
+    virtual int GetVersionValidNumber() = 0;
+    virtual int GetSqliteVersionNumber() = 0;
+};
---
+++ tdd_intro/homework/07_sqlite_header_parser/Mocks.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <gmock/gmock.h>
+
+#include "Interfaces.h"
+
+class GuiMock: public IGui
+{
+public:
+    ~GuiMock(){}
+    MOCK_METHOD0(DisplayHeader, void());
+};
+
+class DbReaderMock:public IDbReader
+{
+public:
+    ~DbReaderMock(){}
+
+    MOCK_METHOD1(ReadFilePath, bool(const std::string filePath));
+    MOCK_METHOD0(CheckHeader, bool());
+    MOCK_METHOD0(GetHeaderString, std::string());
+    MOCK_METHOD0(GetPageSize, int());
+    MOCK_METHOD0(GetFormatWriteVersion, bool());
+    MOCK_METHOD0(GetUnsedBytes, int());
+    MOCK_METHOD0(GetMaximumEmbeddedPayloadFraction, int());
+    MOCK_METHOD0(GetMinimumEmbeddedPayloadFraction, int());
+    MOCK_METHOD0(GetLeafPayloadFraction, int());
+    MOCK_METHOD0(GetFileChangeCounter, int());
+    MOCK_METHOD0(GetPageCount, int());
+    MOCK_METHOD0(GetFirstFreelistPage, int());
+    MOCK_METHOD0(GetFreelistPageCount, int());
+//    40	4	The schema cookie.
+    MOCK_METHOD0(GetSchemaFormat, int());
+    MOCK_METHOD0(GetDefaultPageCacheSize, int());
+    MOCK_METHOD0(GetNumberOfLargestRootPage, int());
+    MOCK_METHOD0(GetDatabaseTextEncoding, int());
+    MOCK_METHOD0(GetUserVersion, int());
+    MOCK_METHOD0(GetIncrementalVacuumMode, bool());
+    MOCK_METHOD0(GetApplicationId, int());
+//    72	20	Reserved for expansion. Must be zero.
+    MOCK_METHOD0(GetVersionValidNumber, int());
+    MOCK_METHOD0(GetSqliteVersionNumber, int());
+};
